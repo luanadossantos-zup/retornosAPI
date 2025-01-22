565b005f97801799ea56ba19b2911cdaf47d5bf9
Luana Silva <luana.dossantos@zup.com.br>
2025-01-22 15:27:07 -0300

    feat: adiciona um GlobalExceptionHandler para validações de Product Entity

--- src/main/java/com/example/retornosAPI/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/retornosAPI/config/exception/GlobalExceptionHandler.java
@@ -1,4 +1,29 @@
 package com.example.retornosAPI.config.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
 }
