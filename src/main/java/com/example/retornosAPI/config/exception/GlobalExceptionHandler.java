27d8164893f73c76294a9700c1ab272418fd6ba1
Luana Silva <luana.dossantos@zup.com.br>
2025-01-22 17:52:12 -0300

    feat: adiciona método para lidar com ConstraintViolationException

--- src/main/java/com/example/retornosAPI/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/retornosAPI/config/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.retornosAPI.config.exception;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,11 +19,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
             errors.put(error.getField(), error.getDefaultMessage());
         }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex) {
+        Map<String, String> errors = ex.getConstraintViolations().stream()
+                .collect(Collectors.toMap(
+                        violation -> violation.getPropertyPath().toString(), // Nome do campo
+                        ConstraintViolation::getMessage // Mensagem de erro
+                ));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
