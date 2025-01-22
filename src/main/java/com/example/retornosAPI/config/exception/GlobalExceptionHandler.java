8537b03c234d2615ad2dd17e4d6d890f681a8efa
Luana Silva <luana.dossantos@zup.com.br>
2025-01-22 17:13:54 -0300

    feat: adiciona notação ao GlobalExceptionHandler

--- src/main/java/com/example/retornosAPI/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/retornosAPI/config/exception/GlobalExceptionHandler.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
