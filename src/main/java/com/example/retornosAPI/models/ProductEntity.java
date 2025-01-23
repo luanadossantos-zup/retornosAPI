bdf4920fdfea00d9419c4a43dd2222100c95ea8b
Luana Silva <luana.dossantos@zup.com.br>
2025-01-23 10:37:35 -0300

    feat: adiciona validação mínima para String description

--- src/main/java/com/example/retornosAPI/models/ProductEntity.java
+++ src/main/java/com/example/retornosAPI/models/ProductEntity.java
@@ -4,10 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 @Entity
 public class ProductEntity {
@@ -23,12 +20,12 @@ public class ProductEntity {
     @DecimalMin(value = "1.0", message = "Price minimum is 1,00!")
     private Double price;
 
+    @Size(min = 1, message = "Description can't be empty")
     @Size(max = 500, message = "Maximum allowed of 500 characters!")
     private String description;
 
     @Min(value = 0, message = "Can't use negative numbers!")
     private int inStockQuantity;
-
 
     private Category category;
 
